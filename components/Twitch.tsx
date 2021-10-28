0f52682fd884fdd86ff8b0455859abf0bdc875d8
Aron Teh <aronteh.chess@gmail.com>
2021-10-28 22:22:55 +0100

    Adjusted Twitch player parameters

--- components/Twitch.tsx
+++ components/Twitch.tsx
@@ -5,11 +5,12 @@ import { TwitchEmbed } from 'react-twitch-embed';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Twitch() {
-  // TODO: Play around with the settings for react-twitch-embed
   return (
     <TwitchEmbed 
       channel="sebseb122"
       id={uuidv4()}
+      allowFullscreen="true"
+      autoplay="false"
       theme="dark"
       height="100%"
       width="100%"
