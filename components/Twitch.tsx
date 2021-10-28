b66bd13812f77cf0b99e9f2f8090a9770156f50c
Aron Teh <aronteh.chess@gmail.com>
2021-10-28 20:17:33 +0100

    Added a Twitch player with react-twitch-embed. Still need to play around with its styling

--- components/Twitch.tsx
+++ components/Twitch.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+// @ts-ignore Unbelievable
+import { TwitchEmbed } from 'react-twitch-embed';
 
 export default function Twitch() {
+  // TODO: Play around with the settings for react-twitch-embed
   return (
-    <div>
-      Hi Twitch
-    </div>
+    <TwitchEmbed 
+      channel="sebseb122"
+      id="sebseb122"
+      theme="dark"
+      height="100%"
+      width="100%"
+      muted
+      onVideoPause={() => console.log(':(')}
+    />
   )
 }
