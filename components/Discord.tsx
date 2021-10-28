cf2a6d813c816ea3be2317ac899bc9f3fdb27620
Aron Teh <aronteh.chess@gmail.com>
2021-10-29 00:22:16 +0100

    Added a Discord component. This one looks reasonably friendly

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useTheme } from 'next-themes'
 
 export default function Discord() {
+  const { theme } = useTheme();
+
+  const urlSource:string = `https://discord.com/widget?id=729302391723393057&theme=${theme}` 
+
   return (
-    <div>
-      Hi Discord
-    </div>
+    <iframe 
+      className='rounded-xl'
+      src={urlSource}
+      width="100%" 
+      height="100%" 
+      allowTransparency={true} 
+      frameBorder="0" 
+      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+    />
   )
 }
