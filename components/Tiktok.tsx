3f0a7abedbd10889ab8839f18aa23ed66036e8c5
Aron Teh <aronteh.chess@gmail.com>
2021-10-28 23:01:24 +0100

    Added TikTok widget, but it's a bit dodge to say the least

--- components/Tiktok.tsx
+++ components/Tiktok.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Tiktok() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://apps.elfsight.com/p/platform.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
-    <div>
-      Hi Tiktok
-    </div>
+    <>
+      <div className="elfsight-app-42580575-5b36-4fde-bad3-176c8a5ae76e"></div>
+    </>
   )
 }
