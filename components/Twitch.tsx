98213caab2869d12343c6459db5e8ab39322cf60
Aron Teh <aronteh.chess@gmail.com>
2021-10-29 19:54:15 +0100

    Removing dynamic twitch chat loading functionality for now since NextJS doesn't like the screen object

--- components/Twitch.tsx
+++ components/Twitch.tsx
@@ -7,27 +7,19 @@ import { useTheme } from 'next-themes';
 export default function Twitch() {
   // Grab the theme and window dimensions to decide whether chat should be displayed
   const { theme } = useTheme();
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
+
   // Width is relative to the device orientation
   // Primary means right way up, in landscape it means button on right
-  const { width } = getWindowDimensions();
+  // Not using this for now since screen object and next.js don't like each other
 
-  const [orientation, setOrientation] = useState("");
-
-  useEffect(() => {
-    setOrientation(screen.orientation.type);
-  }, [])
-
-  const portrait:boolean = orientation === "portrait-primary" || orientation === "portrait-secondary";
-  const landscape:boolean = orientation === "landscape-primary" || orientation === "landscape-secondary";
-  // Show chat only if width is sufficiently large
-  const withChat:boolean = (portrait && width > 425) || (landscape && width > 812); 
+  // function getWindowDimensions() {
+  //   const { innerWidth: width, innerHeight: height } = window;
+  //   return {
+  //     width,
+  //     height
+  //   };
+  // }
+  // const { width } = getWindowDimensions();
 
   return (
     <TwitchEmbed 
@@ -38,7 +30,7 @@ export default function Twitch() {
       theme={theme}
       height="100%"
       width="100%"
-      withChat={withChat}
+      withChat={true}
       muted
     />
   )
